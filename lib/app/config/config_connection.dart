9f3f8d7c0247e6b5d3c43feb661e3dc0281aa989
Michal <wasyl144@outlook.com>
2021-07-26 00:44:19 +0200

    changed credentials for auth

--- lib/app/config/config_connection.dart
+++ lib/app/config/config_connection.dart
@@ -1,5 +1,5 @@
 class ConnectionConfig {
-  static const API_URL = "http://192.168.0.66:8000/api/";
+  static const API_URL = "https://test.wasyl144.tk/api/";
   static const API_LOGIN_ROUTE = "login";
   static const API_USER_ROUTE = "user";
 }
